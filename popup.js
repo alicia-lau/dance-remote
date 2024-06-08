bc0a8ce7eabb832999a7e4fdb780cf148baa394b
nfelicidario <nafelicidario@gmail.com>
2024-06-08 07:16:47 -0400

    Set up UI

--- popup.js
+++ popup.js
@@ -1,3 +1,20 @@
+/* 
+MVP TO DO:
+- Add Speed Functionality
+- Add Loop Functionality
+- Add storage call to set up API based on previous inputs
+*/
+
+/*
+STRETCH TO DO:
+  - Add Speed Icon Color Change
+  - Add Loop Icon Color Change
+  - Add Tooltips
+*/
+
+
+
+// Mirror Button Functionality
 document.getElementById('mirrorButton').addEventListener('click', () => {
   chrome.runtime.sendMessage({ action: 'mirror' }, (response) => {
     if (chrome.runtime.lastError) {
@@ -6,4 +23,12 @@ document.getElementById('mirrorButton').addEventListener('click', () => {
       console.log('Message sent successfully:', response);
     }
   });
+  // Change Icon Color
+  const red = getComputedStyle(document.querySelector(":root")).getPropertyValue("--red");
+  const gray = getComputedStyle(document.querySelector(":root")).getPropertyValue("--gray");
+  if (document.getElementById("mirror-icon-container").style.backgroundColor === red || document.getElementById("mirror-icon-container").style.backgroundColor === "rgb(255, 71, 87)") {
+    document.getElementById("mirror-icon-container").style.backgroundColor = gray;
+  } else {
+    document.getElementById("mirror-icon-container").style.backgroundColor = red;
+  }
 });
